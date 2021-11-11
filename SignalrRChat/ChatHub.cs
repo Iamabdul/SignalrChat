39cd61965079a011631a106e1c504f29e400e2bb
Abdul Al-haj <a.alhaj@hotmail.co.uk>
2021-11-11 13:27:50 +0000

    More updates

--- SignalrRChat/ChatHub.cs
+++ SignalrRChat/ChatHub.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
 using SignalrRChat.Api;
 
 namespace SignalrRChat
 {
-    public  class ChatHub : Hub<IChatClient>
+    public class ChatHub : Hub<IChatClient>
     {
-        public async Task SendMessage(ChatMessage message)
-        {
-            Console.WriteLine($"Heeeeeey SendMessage executed with this message: user = {message.User}, message = {message.Message}");
-            Console.WriteLine("Server: Preparing to execute ReceiveMessage...........................................");
-            await Clients.All.ReceiveMessage(message);
-        }
 
-        public class ChatMessage
-        {
-            public string User { get; set; }
+    }
 
-            public string Message { get; set; }
-        }
+    public class ChatMessage
+    {
+        public string User { get; set; }
+
+        public string Message { get; set; }
     }
 }
